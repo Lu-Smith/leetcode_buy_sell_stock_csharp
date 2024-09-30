91c4253c7e65c92473eafaed974f8beb941fed10
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-30 11:18:28 +0100

    Updated Solutions.cs

--- LeetCode/Solutions.cs
+++ LeetCode/Solutions.cs
@@ -7,6 +7,17 @@ namespace LeetCode
 {
     public class Solutions
     {
-        
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if(prices[i] > prices[i - 1])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
     }
 }
